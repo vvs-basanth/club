1167dd67983a63959b496a27d7c356adc6dcdf61
Ibrahim Chikani <147825977+IbrahimDev00@users.noreply.github.com>
2024-09-22 20:59:51 +0530

    Added images and fixed bg-color

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -17,13 +17,13 @@ export function AuroraBackgroundDemo() {
         }}
         className="relative flex flex-col gap-4 items-center justify-center px-4"
       >
-        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+        <div className="text-3xl md:text-7xl font-bold text-zinc-300 text-center">
           Welcome to GITAM Aero Astro Club
         </div>
-        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
+        <div className="font-extralight text-base md:text-4xl text-neutral-200 py-4">
           Grow with us!
         </div>
-        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-white px-4 py-2">
           <a href="/jobs">
           Get Free Membership
           </a>
